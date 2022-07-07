afc2d308657831ac9a83e98ef4b966228144a79e
Simran Preet Singh <simran5590@gmail.com>
2022-07-07 16:32:05 -0700

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Simran Preet Narang on 2022-07-07.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
