3559bf2b6a4bb7611360e38c573b584f035eae3a
Simran Preet Singh <simran5590@gmail.com>
2022-07-08 15:29:01 -0700

    Added MapView

--- DubDubGrub/LocationMapView.swift
+++ DubDubGrub/LocationMapView.swift
@@ -6,10 +6,28 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationMapView: View {
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054),
+                                                   span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    
     var body: some View {
-        Text("Location Map View")
+        ZStack {
+            Map(coordinateRegion: $region)
+                .ignoresSafeArea(.all, edges: .top)
+            
+            VStack {
+                Image("ddg-map-logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 70)
+                    .shadow(radius: 10)
+                Spacer()
+            }
+            
+        }
     }
 }
 
